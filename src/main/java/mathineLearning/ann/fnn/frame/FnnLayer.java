01a8caecb8405537c244b6dda62d5a81db09c8ec
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2016-12-08 19:18:15 +0800

    增加fnn实现的代码

---
+++ src/main/java/mathineLearning/ann/fnn/frame/FnnLayer.java
@@ -0,0 +1,34 @@
+package mathineLearning.ann.fnn.frame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiwen.yxw on 2016/12/8.
+ *
+ */
+public class FnnLayer {
+    /**
+     *
+     */
+    private List<FnnNode> nodeList;
+
+    public List<Double> inputToOutput(List<Double> lst) {
+        List<Double> result = new ArrayList<Double>();
+        for (FnnNode fnnNode : nodeList) {
+            Double tmp = fnnNode.inputToOutput(lst);
+            if (tmp != null) {
+                result.add(tmp);
+            }
+        }
+        return result;
+    }
+
+    public List<FnnNode> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<FnnNode> nodeList) {
+        this.nodeList = nodeList;
+    }
+}
---
+++ src/main/java/mathineLearning/ann/neuron/impl/TangentThreshold.java
@@ -0,0 +1,21 @@
+package mathineLearning.ann.neuron.impl;
+
+
+import mathineLearning.ann.neuron.IThreshold;
+
+/**
+ * Created by xiwen.yxw on 2016/12/8.
+ */
+public class TangentThreshold implements IThreshold {
+    @Override
+    public double activate(double x) {
+        return Math.tanh(x);
+    }
+
+    @Override
+    public double derivative(double x) {
+        double coshX = Math.cosh(x);
+        double denOm = (Math.cosh(2*x) + 1);
+        return 4*coshX*coshX / (denOm*denOm);
+    }
+}
